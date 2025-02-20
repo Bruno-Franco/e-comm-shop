858fddf8118d5bbe3a47b7a2fbbccfdabd61f879
Bruno Frasan <brunofrancco@gmail.com>
2025-02-20 09:54:31 +0000

    basic routes

---
+++ src/routes/navigation/navigation.component.jsx
@@ -0,0 +1,24 @@
+import { Fragment } from 'react' // fragment permite criar um elemento nao renderizado na pagina
+import { Outlet, Link } from 'react-router'
+import { ReactComponent as CrwLogo } from '../../assets/crown.svg' // importa um 'logo' como um componente diretamente
+import './navigation.styles.scss'
+
+function Navigation() {
+	return (
+		<Fragment>
+			<div className='navigation'>
+				<Link className='logo-container' to='/'>
+					<CrwLogo className='logo' />
+				</Link>
+				<div className='nav-links-container'>
+					<Link className='nav-link' to='/shop'>
+						SHOP
+					</Link>
+				</div>
+			</div>
+			<Outlet />
+		</Fragment>
+	)
+}
+
+export default Navigation
